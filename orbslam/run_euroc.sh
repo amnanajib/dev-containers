8770446bac86917db956e32d4f694cee48b30504
Atanas Mirchev <taimir93@gmail.com>
2019-03-21 17:48:34 +0100

    Add opengl to the orbslam container.

---
+++ orbslam/run_euroc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./Examples/Monocular/mono_euroc Vocabulary/ORBvoc.txt Examples/Monocular/EuRoC.yaml /root/DATA/euroc/MH_05_difficult/mav0/cam0/data Examples/Monocular/EuRoC_TimeStamps/SEQUENCE.txt
